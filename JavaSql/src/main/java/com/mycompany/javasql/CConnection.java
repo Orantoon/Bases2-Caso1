bea6e81a58e6921fe146fabd23957296dd5be60c
Orantoon <71191565+Orantoon@users.noreply.github.com>
2022-02-27 22:35:02 -0600

    Java

--- JavaSql/src/main/java/com/mycompany/javasql/CConnection.java
+++ JavaSql/src/main/java/com/mycompany/javasql/CConnection.java
@@ -2,6 +2,8 @@ package com.mycompany.javasql;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import javax.swing.JOptionPane;
 
 /*
@@ -18,24 +20,79 @@ public class CConnection {
     
     Connection connect = null;
     
-    String user = "usersql";
-    String password = "root";
+    String user = "sa";
+    String password = "CyberTiger#19";
     String db = "bases2_caso1";
     String ip = "localhost\\DEIBID";
     String port = "1433";
-    
-    String chain = "jdbc:sqlserver://"+ip+":"+port+"/"+db;
-    // "jdbc:sqlserver://"+ip+":"+port+";"+"databaseName="+db;
     
     public Connection startConnection(){
         try{
             String chain = "jdbc:sqlserver://"+ip+":"+port+";databaseName="+db;
             connect = DriverManager.getConnection(chain,user,password);
-
-            JOptionPane.showMessageDialog(null, "Se ha conectado correctamente");
+            
+            //JOptionPane.showMessageDialog(null, "Se ha conectado correctamente");
+            
         } catch (Exception e){
             JOptionPane.showMessageDialog(null, "Error al conectarse con la base de datos. Error: "+ e.toString());
         }
         return connect;
+    }
+    
+    public void query1(){
+        String bases = "";
+        
+        try{
+            Statement sql = startConnection().createStatement();
+            String consulta = "EXEC bases2_caso1.dbo.Query1 @canton = 7";
+            ResultSet resultado = sql.executeQuery(consulta);
+            
+            while(resultado.next()){
+                bases += resultado.getString(2) + "\n"; //entregableId
+            }
+            
+            JOptionPane.showMessageDialog(null, bases);
+            
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Error: "+ e.toString());
+        }
+    }
+    
+    public void query2(){
+        String bases = "";
+        
+        try{
+            Statement sql = startConnection().createStatement();
+            String consulta = "EXEC bases2_caso1.dbo.Query2";
+            ResultSet resultado = sql.executeQuery(consulta);
+            
+            while(resultado.next()){
+                bases += resultado.getString(1) + "\n";
+            }
+            
+            JOptionPane.showMessageDialog(null, bases);
+            
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Error: "+ e.toString());
+        }
+    }
+    
+    public void query3(){
+        String bases = "";
+        
+        try{
+            Statement sql = startConnection().createStatement();
+            String consulta = "EXEC bases2_caso1.dbo.Query3";
+            ResultSet resultado = sql.executeQuery(consulta);
+            
+            while(resultado.next()){
+                bases += resultado.getString(1) + "\n";
+            }
+            
+            JOptionPane.showMessageDialog(null, bases);
+            
+        } catch (Exception e){
+            JOptionPane.showMessageDialog(null, "Error: "+ e.toString());
+        }
     }
 }
--- JavaSql/src/main/java/com/mycompany/javasql/main.java
+++ JavaSql/src/main/java/com/mycompany/javasql/main.java
@@ -1,5 +1,11 @@
 package com.mycompany.javasql;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -13,6 +19,10 @@ package com.mycompany.javasql;
 public class main {
     public static void main(String[] args){
         CConnection connection = new CConnection();
-        connection.startConnection();
+        //connection.startConnection();
+        
+        connection.query1();
+        connection.query2();
+        connection.query3();
     }
 }
